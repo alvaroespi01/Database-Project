9c0aee813c51748fc167cb31675ac9ca78690989
Alvaro Espinoza <espinalv@classwork.engr.oregonstate.edu>
2026-03-07 22:58:39 -0800

    Updating Movies, Customers, Screens to include update and add. Updating app.js to handle new functionality. Updating plsql.sql to add new stored procedures.

--- project/app.js
+++ project/app.js
@@ -37,12 +37,14 @@ app.get('/Movies', async function (req, res) {
         // Create and execute our queries
         const movieQuery = `SELECT movieID, title, genre, duration 
                 FROM Movies ORDER BY title ASC`;
-                
+        const movieDropdown = `SELECT * FROM Movies ORDER BY movieID ASC`;
+        
+        const [movieList] = await db.query(movieDropdown);
         const [movie] = await db.query(movieQuery);
 
         // Render the bsg-people.hbs file, and also send the renderer
         //  an object that contains our bsg_people and bsg_homeworld information
-        res.render('Movies/Movies', { movie: movie });
+        res.render('Movies/Movies', { movie: movie, movieList:movieList });
     } catch (error) {
         console.error('Error executing queries:', error);
         // Send a generic error message to the browser
@@ -56,7 +58,7 @@ app.get('/Screens', async function (req, res) {
     try {
         // Create and execute our queries
         const screensQuery = `SELECT screenID, screenNumber, seatingCapacity 
-                FROM Screens ORDER BY seatingCapacity DESC`;
+                FROM Screens ORDER BY screenID ASC`;
 
         const [screen] = await db.query(screensQuery);
 
@@ -74,14 +76,11 @@ app.get('/Screens', async function (req, res) {
 
 app.get('/Customers', async function (req, res) {
     try {
-        // Create and execute our queries
         const customersQuery = `SELECT customerID, name, email 
                 FROM Customers ORDER BY name ASC`;
 
         const [customer] = await db.query(customersQuery);
 
-        // Render the bsg-people.hbs file, and also send the renderer
-        //  an object that contains our bsg_people and bsg_homeworld information
         res.render('Customers/Customers', { customer: customer });
     } catch (error) {
         console.error('Error executing queries:', error);
@@ -152,18 +151,6 @@ app.get('/Tickets', async function (req, res) {
     }
 });
 
-app.get('/EditScreen', async function (req, res){
-try {
-        res.render('Screens/EditScreen', { });
-    } catch (error) {
-        console.error('Error executing queries:', error);
-        // Send a generic error message to the browser
-        res.status(500).send(
-            'An error occurred while executing the database queries.'
-        );
-    }
-});
-
 app.get('/EditCustomer', async function (req, res){
 try {
         res.render('Customers/EditCustomer', { });
@@ -214,6 +201,263 @@ try {
     }
 });
 
+app.post('/Movies/addMovie', async function (req, res) {
+    try {
+        // Parse frontend form information
+        let data = req.body;
+
+        // Cleanse data - If the duration isn't a number, make it NULL.
+        if (isNaN(parseInt(data.create_person_duration)))
+            data.create_person_duration = null;
+
+        // Create and execute our queries
+        // Using parameterized queries (Prevents SQL injection attacks)
+        const query1 = `CALL sp_CreateMovie(?, ?, ?, @new_id);`;
+
+        // Store ID of last inserted row
+        const [[[rows]]] = await db.query(query1, [
+            data.create_movie_title,
+            data.create_movie_genre,
+            data.create_movie_duration,
+        ]);
+
+        console.log(`CREATE Movie. ID: ${rows.new_id} ` +
+            `Title: ${data.create_movie_title}`
+        );
+
+        // Redirect the user to the updated webpage
+        res.redirect('/Movies');
+    } catch (error) {
+        console.error('Error executing queries:', error);
+        // Send a generic error message to the browser
+        res.status(500).send(
+            'An error occurred while executing the database queries.'
+        );
+    }
+});
+
+app.post('/Movies/update', async function (req, res) {
+    try {
+        // Parse frontend form information
+        const data = req.body;
+
+        // Cleanse data - If the duration isn't a number, make it NULL.
+        if (isNaN(parseInt(data.update_movie_duration)))
+            data.update_movie_duration = null;
+
+        // Create and execute our query
+        // Using parameterized queries (Prevents SQL injection attacks)
+        const query1 = 'CALL sp_UpdateMovie(?, ?, ?, ?);';
+        const query2 = 'SELECT title, genre, duration FROM Movies WHERE movieID = ?;';
+        await db.query(query1, [
+            data.update_movie,
+            data.update_movie_title,
+            data.update_movie_genre,
+            data.update_movie_duration,
+        ]);
+        const [[rows]] = await db.query(query2, [data.update_movie]);
+
+        console.log(`UPDATE Movie. ID: ${data.update_movie} ` +
+            `Title: ${rows.title}`
+        );
+
+        // Redirect the user to the updated webpage data
+        res.redirect('/Movies');
+    } catch (error) {
+        console.error('Error executing queries:', error);
+        // Send a generic error message to the browser
+        res.status(500).send(
+            'An error occurred while executing the database queries.'
+        );
+    }
+});
+
+// DELETE ROUTES
+app.post('/Movies/delete', async function (req, res) {
+    try {
+        // Parse frontend form information
+        let data = req.body;
+
+        // Create and execute our query
+        // Using parameterized queries (Prevents SQL injection attacks)
+        const query1 = `CALL sp_DeleteMovie(?);`;
+        await db.query(query1, [data.delete_movie_id]);
+
+        console.log(`DELETE Movie. ID: ${data.delete_movie_id} ` +
+            `Name: ${data.delete_movie_title}`
+        );
+
+        // Redirect the user to the updated webpage data
+        res.redirect('/Movies');
+    } catch (error) {
+        console.error('Error executing queries:', error);
+        // Send a generic error message to the browser
+        res.status(500).send(
+            'An error occurred while executing the database queries.'
+        );
+    }
+});
+
+app.post('/Screens/addScreen', async function (req, res) {
+    try {
+        let data = req.body;
+
+        const query1 = `CALL sp_CreateScreen(?, ?, @new_id);`;
+
+        const [[[rows]]] = await db.query(query1, [
+            data.create_screen_num,
+            data.create_screen_capacity,
+        ]);
+
+        console.log(`CREATE Screen. ID: ${rows.new_id} ` +
+            `Title: ${data.create_screen_num}`
+        );
+
+        res.redirect('/Screens');
+    } catch (error) {
+        if (error.code === 'ER_DUP_ENTRY') {
+            const screensQuery = `SELECT screenID, screenNumber, seatingCapacity 
+                FROM Screens ORDER BY screenID ASC`;
+
+            const [screen] = await db.query(screensQuery);
+
+            return res.render('Screens/Screens', {
+                screen: screen,
+                addError: "Screen number already exists.",
+                layout: 'main'
+            });     
+        }
+
+        console.error('Error executing queries:', error);
+        res.status(500).send(
+            'An error occurred while executing the database queries.'
+        );
+    }
+});
+
+app.post('/Screens/update', async function (req, res){
+try {
+        const data = req.body;
+
+        const query1 = 'CALL sp_UpdateScreen(?, ?, ?);';
+        const query2 = 'SELECT screenNumber, seatingCapacity FROM Screens WHERE screenID = ?;';
+        await db.query(query1, [
+            data.update_screen,
+            data.update_screen_num,
+            data.update_screen_capacity,
+        ]);
+        const [[rows]] = await db.query(query2, [data.update_screen]);
+
+        console.log(`UPDATE Screen. ID: ${data.update_screen} ` + `Title: ${rows.screenNumber}`
+        );
+
+        res.redirect('/Screens');
+    } catch (error) {
+        if (error.code === 'ER_DUP_ENTRY') {
+            const screensQuery = `SELECT screenID, screenNumber, seatingCapacity 
+                FROM Screens ORDER BY screenID ASC`;
+
+            const [screen] = await db.query(screensQuery);
+
+            return res.render('Screens/Screens', {
+                screen: screen,
+                updateError: "Screen number already exists.",
+                layout: 'main'
+            });     
+        }
+
+        console.error('Error executing queries:', error);
+        res.status(500).send(
+            'An error occurred while executing the database queries.'
+        );
+    }
+});
+
+
+
+app.post('/Customers/addCustomer', async function (req, res) {
+    try {
+        let data = req.body;
+
+        const query1 = `CALL sp_CreateCustomer(?, ?, @new_id);`;
+
+        const [[[rows]]] = await db.query(query1, [
+            data.create_name,
+            data.create_email,
+        ]);
+
+        res.redirect('/Customers');
+    } catch (error) {
+        if (error.code === 'ER_DUP_ENTRY') {
+            const customersQuery = `SELECT customerID, name, email 
+                FROM Customers ORDER BY name ASC`;
+
+            const [customer] = await db.query(customersQuery);
+
+            return res.render('Customers/Customers', {
+                customer: customer,
+                addError: "Email already exists.",
+                layout: 'main'
+            });     
+        }
+
+        console.error('Error executing queries:', error);
+        res.status(500).send(
+            'An error occurred while executing the database queries.'
+        );
+    }
+});
+
+app.post('/Customers/update', async function (req, res){
+try {
+        const data = req.body;
+
+        const query1 = 'CALL sp_UpdateCustomer(?, ?, ?);';
+        const query2 = 'SELECT name, email FROM Customers WHERE customerID = ?;';
+        await db.query(query1, [
+            data.update_customer,
+            data.update_name,
+            data.update_email,
+        ]);
+        const [[rows]] = await db.query(query2, [data.update_customer]);
+
+        res.redirect('/Customers');
+    } catch (error) {
+        if (error.code === 'ER_DUP_ENTRY') {
+            const customersQuery = `SELECT customerID, name, email 
+                FROM Customers ORDER BY name ASC`;
+
+            const [customer] = await db.query(customersQuery);
+
+            return res.render('Customers/Customers', {
+                customer: customer,
+                updateError: "Email already exists.",
+                layout: 'main'
+            });     
+        }
+
+        console.error('Error executing queries:', error);
+        res.status(500).send(
+            'An error occurred while executing the database queries.'
+        );
+    }
+});
+
+
+app.get('/reset-table', async function (req, res) {
+        console.log("Reset table route hit"); // add this
+    try {
+        const query1 = 'CALL sp_load_moviesdb();';
+        await db.query(query1);
+        res.redirect(req.headers.referer || '/');
+    } catch (error) {
+        console.error("Error executing PL/SQL:", error);
+        // Send a generic error message to the browser
+        res.status(500).send("An error occurred while executing the PL/SQL.");
+    }
+});
+
+
 // ########################################
 // ########## LISTENER
 
@@ -224,3 +468,4 @@ app.listen(PORT, function () {
             '; press Ctrl-C to terminate.'
     );
 });
+
---
+++ project/plsql.sql
@@ -0,0 +1,151 @@
+-- #############################
+-- CREATE Moviee
+-- #############################
+DROP PROCEDURE IF EXISTS sp_CreateMovie;
+
+DELIMITER //
+CREATE PROCEDURE sp_CreateMovie(
+    IN m_title VARCHAR(145), 
+    IN m_genre VARCHAR(145), 
+    IN m_duration INT, 
+    OUT m_id INT)
+BEGIN
+    INSERT INTO Movies (title, genre, duration) 
+    VALUES (m_title, m_genre, m_duration);
+
+    -- Store the ID of the last inserted row
+    SELECT LAST_INSERT_ID() into m_id;
+    -- Display the ID of the last inserted movie.
+    SELECT LAST_INSERT_ID() AS 'new_id';
+
+    -- Example of how to get the ID of the newly created person:
+        -- CALL sp_CreatePerson('Theresa', 'Evans', 2, 48, @new_id);
+        -- SELECT @new_id AS 'New Person ID';
+END //
+DELIMITER ;
+
+
+-- #############################
+-- UPDATE bsg_people
+-- #############################
+DROP PROCEDURE IF EXISTS sp_UpdateMovie;
+
+DELIMITER //
+CREATE PROCEDURE sp_UpdateMovie(IN m_id INT, IN m_title VARCHAR(145), IN m_genre VARCHAR(145), IN m_duration INT)
+
+BEGIN
+    UPDATE Movies SET title = m_title, genre = m_genre, duration = m_duration WHERE movieID = m_id; 
+END //
+DELIMITER ;
+
+
+-- #############################
+-- DELETE bsg_people
+-- #############################
+DROP PROCEDURE IF EXISTS sp_DeleteMovie;
+
+DELIMITER //
+CREATE PROCEDURE sp_DeleteMovie(IN m_id INT)
+BEGIN
+    DECLARE error_message VARCHAR(255); 
+
+    -- error handling
+    DECLARE EXIT HANDLER FOR SQLEXCEPTION
+    BEGIN
+        -- Roll back the transaction on any error
+        ROLLBACK;
+        -- Propogate the custom error message to the caller
+        RESIGNAL;
+    END;
+
+    START TRANSACTION;
+        -- Deleting corresponding rows from both bsg_people table and 
+        --      intersection table to prevent a data anamoly
+        -- This can also be accomplished by using an 'ON DELETE CASCADE' constraint
+        --      inside the bsg_cert_people table.
+        DELETE FROM Showtimes WHERE movieID = m_id;
+        DELETE FROM Movies WHERE movieID = m_id;
+
+        -- ROW_COUNT() returns the number of rows affected by the preceding statement.
+        IF ROW_COUNT() = 0 THEN
+            set error_message = CONCAT('No matching record found in Movies for id: ', m_id);
+            -- Trigger custom error, invoke EXIT HANDLER
+            SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = error_message;
+        END IF;
+
+    COMMIT;
+
+END //
+DELIMITER ;
+
+
+-- #############################
+-- UPDATE Screen
+-- #############################
+DROP PROCEDURE IF EXISTS sp_UpdateScreen;
+
+DELIMITER //
+CREATE PROCEDURE sp_UpdateScreen(IN s_id INT, IN s_screenNumber INT, IN m_seatingCapacity INT)
+
+BEGIN
+    UPDATE Screens SET screenNumber = s_screenNumber, seatingCapacity = m_seatingCapacity WHERE screenID = s_id; 
+END //
+DELIMITER ;
+
+-- #############################
+-- CREATE Moviee
+-- #############################
+DROP PROCEDURE IF EXISTS sp_CreateScreen;
+
+DELIMITER //
+CREATE PROCEDURE sp_CreateScreen(
+    IN m_screenNumber INT, 
+    IN m_seatingCapacity INT, 
+    OUT m_id INT)
+BEGIN
+    INSERT INTO Screens (screenNumber, seatingCapacity) 
+    VALUES (m_screenNumber, m_seatingCapacity);
+
+    -- Store the ID of the last inserted row
+    SELECT LAST_INSERT_ID() into m_id;
+    -- Display the ID of the last inserted movie.
+    SELECT LAST_INSERT_ID() AS 'new_id';
+
+END //
+DELIMITER ;
+
+
+
+-- #############################
+-- UPDATE Customer
+-- #############################
+DROP PROCEDURE IF EXISTS sp_UpdateCustomer;
+
+DELIMITER //
+CREATE PROCEDURE sp_UpdateCustomer(IN c_id INT, IN c_name varchar(145), IN c_email varchar(145))
+
+BEGIN
+    UPDATE Customers SET name = c_name, email = c_email WHERE customerID = c_id; 
+END //
+DELIMITER ;
+
+-- #############################
+-- CREATE Customer
+-- #############################
+DROP PROCEDURE IF EXISTS sp_CreateCustomer;
+
+DELIMITER //
+CREATE PROCEDURE sp_CreateCustomer(
+    IN c_name VARCHAR(145), 
+    IN c_email VARCHAR(145), 
+    OUT c_id INT)
+BEGIN
+    INSERT INTO Customers (name, email) 
+    VALUES (c_name, c_email);
+
+    SELECT LAST_INSERT_ID() into c_id;
+    SELECT LAST_INSERT_ID() AS 'new_id';
+
+END //
+DELIMITER ;
+
